950f6ccd00fa755a5a81ea4d697b764c939fd8c5
efeuentertainment <fire@whatimean.ch>
2021-10-23 11:54:19 +0200

    language selection to trigger.sh

--- trigger.sh
+++ trigger.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
 #run as root
-pkill -x -o -SIGUSR1 -f 'node /usr/local/aws-polly/signal.js -a plughw:1 -m -v Matthew' || node /usr/local/aws-polly/signal.js -a plughw:1 -m -v Matthew &
+if [ $# -eq 0 ]; then
+	voice="Matthew"
+else
+	voice="$1"
+fi
+#echo 'hello '$voice
+pkill -x -o -SIGUSR1 -f 'node /usr/local/aws-polly/signal.js -a plughw:1 -m -v '$voice || node /usr/local/aws-polly/signal.js -a plughw:1 -m -v $voice &
